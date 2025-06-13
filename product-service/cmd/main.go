1ab89c6155619f8f89b458f6d25a42bbccc78f6b
Dimitr <khakimoff.dima@mail.ru>
2025-06-13 19:38:29 +0300

    new pgx sql driver added

--- product-service/cmd/main.go
+++ product-service/cmd/main.go
@@ -1,33 +1,43 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"log"
+
 	"github.com/Gergenus/commerce/product-service/internal/config"
-	handlers "github.com/Gergenus/commerce/product-service/internal/handler"
+	"github.com/Gergenus/commerce/product-service/internal/models"
 	"github.com/Gergenus/commerce/product-service/internal/repository"
-	"github.com/Gergenus/commerce/product-service/internal/service"
 	dbpkg "github.com/Gergenus/commerce/product-service/pkg/db"
-	"github.com/Gergenus/commerce/product-service/pkg/logger"
-	"github.com/labstack/echo/v4"
-	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
 	cfg := config.InitConfig()
 	db := dbpkg.InitDB(cfg.PostgresURL)
-	log := logger.SetupLogger(cfg.LogLevel)
+	defer db.DB.Close(context.Background())
+	// log := logger.SetupLogger(cfg.LogLevel)
 
-	repo := repository.NewPostgresRepository(db, log)
-	serv := service.NewProductService(log, &repo)
-	hand := handlers.NewProductHandler(&serv)
-	e := echo.New()
+	repo := repository.NewPostgresRepository(db)
+	// serv := service.NewProductService(log, &repo)
+	// hand := handlers.NewProductHandler(&serv)
+	// e := echo.New()
 
-	e.Use(middleware.Logger())
-	e.Use(middleware.Recover())
-	group := e.Group("/api/v1/products")
-	{
-		group.POST("/", hand.AddCategory)
+	// e.Use(middleware.Logger())
+	// e.Use(middleware.Recover())
+	// group := e.Group("/api/v1/products")
+	// {
+	// 	group.POST("/", hand.AddCategory)
+	// }
+
+	// e.Start(":" + cfg.HTTPPort)
+	z, err := repo.AddCategory(context.Background(), "SVO")
+	if err != nil {
+		log.Println(err)
 	}
-
-	e.Start(":" + cfg.HTTPPort)
-
+	fmt.Println(z)
+	id, err := repo.CreateProduct(context.Background(), models.Product{ProductName: "ТАНК", Price: 5000.5, SellerID: 5, CategoryID: 1})
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(id)
 }
--- product-service/internal/repository/product_repository_interface.go
+++ product-service/internal/repository/product_repository_interface.go
@@ -8,12 +8,12 @@ import (
 
 type RepositoryInterface interface {
 	AddCategory(ctx context.Context, category string) (int, error)
-	DeleteCategoryByID(ctx context.Context, id int) (int, error)
+	DeleteCategoryByID(ctx context.Context, id int) error
 	GetProductsByCategory(ctx context.Context, category string) ([]models.Product, error)
 	GetStockByID(ctx context.Context, id int) (int, error)
 	AddStockByID(ctx context.Context, id int, number int) (int, error)
 	ReduceStock(ctx context.Context, id int, number int) (int, error)
 	CreateProduct(ctx context.Context, product models.Product) (int, error)
-	DeleteProduct(ctx context.Context, id int) (int, error)
-	UpdateProduct(ctx context.Context, product models.Product) (int, error)
+	DeleteProduct(ctx context.Context, id int) error
+	UpdateProduct(ctx context.Context, product models.Product) error
 }
